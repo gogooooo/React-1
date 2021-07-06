05001152dd5c8d96850d148b9347933b60b6bb13
gogooooo <hagermahmoud1094@gmail.com>
2021-07-06 16:07:39 +0200

    jsx checkpoint

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import {Form, Button} from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
+import imgSrc from './20.jpg';
 function App() {
   return (
-    
-    <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-      
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
-        
-  );
+<>
+      <div style={{ border: 'solid 1px black', maxWidth: '100vw' }}>
+
+        <h1 class="title red">Your name here</h1>
+
+        <br />
+
+        <img src={imgSrc} />
+
+        <br />
+
+        <img src="/19.jpg" />
+        <br />
+
+      <video width="320" height="240" controls/>
+
+      </div>
+
+</>
+
+
+
+      );
 }
 
-export default App;
+      export default App;
